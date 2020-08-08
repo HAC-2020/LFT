0cf6736e3542d4990d3901f1d9fe0ddf8c2426bc
radiantly <44368997+radiantly@users.noreply.github.com>
2020-08-08 13:23:31 +0530

    Add basic signup/login pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import Button from '@material-ui/core/Button';
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { pink } from "@material-ui/core/colors";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: pink,
+    secondary: pink,
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route path="/register">
+            <SignUp />
+          </Route>
+          <Route path="/">
+            <SignIn />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ frontend/src/BasicEntry.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Link from "@material-ui/core/Link";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"A project by "}
+      <Link color="inherit" href="https://github.com/HAC-2020/LFT">
+        Team LFT
+      </Link>
+      {"."}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+}));
+
+export default function BasicEntry(props) {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        {props.children}
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
