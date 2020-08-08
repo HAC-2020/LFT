0965dd9c4185a967ffbf043d9f0379cfb771500d
radiantly <44368997+radiantly@users.noreply.github.com>
2020-08-08 22:10:43 +0530

    Add Bulma CSS

--- frontend/src/SignIn.js
+++ frontend/src/SignIn.js
@@ -8,75 +8,65 @@ import { Link } from "react-router-dom";
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
+import * as axios from "axios";
 
 import BasicEntry from "./BasicEntry";
 
 import Copyright from "./Copyright";
 
-const useStyles = makeStyles(theme => ({
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+class SignIn extends React.Component {
+  login = () => {
+    axios.post("https://lft-hac.herokuapp.com/api/v1/user/login").then(console.log);
+  };
 
-export default function SignIn() {
-  const classes = useStyles();
-
-  return (
-    <BasicEntry>
-      <Typography component="h1" variant="h5">
-        Sign in to Proctor
-      </Typography>
-      <form className={classes.form} noValidate>
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          label="Email Address"
-          name="email"
-          autoComplete="email"
-          autoFocus
-        />
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-        />
-        <FormControlLabel
-          control={<Checkbox value="remember" color="primary" />}
-          label="Remember me"
-        />
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="primary"
-          className={classes.submit}
-        >
-          Sign In
-        </Button>
-        <Grid container>
-          <Grid item xs>
-            <Link to="/forgot">Forgot password?</Link>
+  render() {
+    return (
+      <BasicEntry>
+        <Typography component="h1" variant="h5">
+          Sign in to Proctor
+        </Typography>
+        <form noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button type="submit" fullWidth variant="contained" color="primary">
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link to="/forgot">Forgot password?</Link>
+            </Grid>
           </Grid>
-        </Grid>
-        <Box mt={5}>
-          <Copyright />
-        </Box>
-      </form>
-    </BasicEntry>
-  );
+          <Box mt={5}>
+            <Copyright />
+          </Box>
+        </form>
+      </BasicEntry>
+    );
+  }
 }
+
+export default SignIn;
